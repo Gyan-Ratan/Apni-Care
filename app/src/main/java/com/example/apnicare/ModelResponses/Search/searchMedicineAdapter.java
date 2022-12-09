8dbdf60e1dc153f2f59d1f87d8c655f956736dd4
Harshit jain <harshit2020cs198@abesit.edu.in>
2022-12-09 23:34:17 +0530

    images are added

--- app/src/main/java/com/example/apnicare/ModelResponses/Search/searchMedicineAdapter.java
+++ app/src/main/java/com/example/apnicare/ModelResponses/Search/searchMedicineAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.apnicare.R;
 import com.example.apnicare.RetrofitClient;
 import com.example.apnicare.SharedPrefManager;
@@ -27,6 +29,7 @@ public class searchMedicineAdapter extends RecyclerView.Adapter<searchMedicineAd
     private Context context;
     private List<items> Items;
     SharedPrefManager sharedPrefManager;
+
 
     public searchMedicineAdapter(Context context, List<items> items) {
         this.context = context;
@@ -44,10 +47,23 @@ public class searchMedicineAdapter extends RecyclerView.Adapter<searchMedicineAd
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String phUrl="https://res.cloudinary.com/du8msdgbj/image/upload/w_360,h_360,c_fit,a_ignore,q_60,fl_lossy,f_auto/v1573463739/nherof9d47s0wgmmrrzh.png";
         items searchresponse=Items.get(position);
 
         holder.name.setText(Items.get(position).getName());
         holder.price.setText(Items.get(position).getMrp().toString());
+        holder.OTC.setText(searchresponse.getPrescriptionRequired().toString());
+        String url =searchresponse.getImage().getOriginalPath();
+        if (url==null){
+            Toast.makeText(context,"no image",Toast.LENGTH_SHORT).show();
+
+        }
+        else {
+            Glide.with(context)
+                    .load(url)
+                    .into(holder.imageView);
+
+        }
     }
 
 
@@ -60,8 +76,9 @@ public class searchMedicineAdapter extends RecyclerView.Adapter<searchMedicineAd
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView name;
-        TextView price;
+        TextView price,OTC;
         Button add;
+        ImageView imageView;
         private LinearLayout Quantitybtn;
 
         public ViewHolder(@NonNull View itemView) {
@@ -70,6 +87,8 @@ public class searchMedicineAdapter extends RecyclerView.Adapter<searchMedicineAd
             name=itemView.findViewById(R.id.name_medicine);
             price=itemView.findViewById(R.id.price);
             add=itemView.findViewById(R.id.addtocartsearchbtn);
+            imageView=itemView.findViewById(R.id.searchImage);
+            OTC=itemView.findViewById(R.id.searchOTC);
             Quantitybtn=itemView.findViewById(R.id.quantitybtn);
             add.setOnClickListener(this);
         }
