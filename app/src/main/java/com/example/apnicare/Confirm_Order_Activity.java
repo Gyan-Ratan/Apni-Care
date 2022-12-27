c836757432429eea89d2cba57d2f9912068ca3e3
Harsh Tyagi <91258683+Mysharsh@users.noreply.github.com>
2022-12-28 03:04:31 +0530

    orderconfirm

--- app/src/main/java/com/example/apnicare/Confirm_Order_Activity.java
+++ app/src/main/java/com/example/apnicare/Confirm_Order_Activity.java
@@ -7,10 +7,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.apnicare.AllAdapters.ConfirmOrderAdapter;
+import com.example.apnicare.AllProducts.OrderBookedActivity;
 import com.example.apnicare.ModelResponses.OrderCart.CartBookingResponse;
+import com.example.apnicare.YourOrders.YourOrderConfirmedActivity;
 import com.example.apnicare.myCart.CartActivity;
 import com.example.apnicare.myCart.CartItemAdapter;
 
@@ -21,17 +24,23 @@ import retrofit2.Response;
 public class Confirm_Order_Activity extends AppCompatActivity {
     int order;
     RecyclerView recyclerView;
+    Button confirm;
     SharedPrefManager sharedPrefManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_order);
         recyclerView=findViewById(R.id.recy);
+        confirm=findViewById(R.id.confirm_button);
         sharedPrefManager=new SharedPrefManager(this);
         Intent intent=getIntent();
 //        int id= Integer.parseInt(intent.getStringExtra("id"));
         order= intent.getIntExtra("order",0);
         confirmOrder();
+        confirm.setOnClickListener(view -> {
+            Intent intent1 = new Intent(getApplicationContext(), YourOrderConfirmedActivity.class);
+            startActivity(intent1);
+        });
 
     }
 
@@ -43,7 +52,6 @@ public class Confirm_Order_Activity extends AppCompatActivity {
                 CartBookingResponse cartBookingResponse=response.body();
                 Toast.makeText(Confirm_Order_Activity.this,response.toString(),Toast.LENGTH_SHORT).show();
                 if (response.isSuccessful()){
-
 
                 ConfirmOrderAdapter adapter =new ConfirmOrderAdapter(Confirm_Order_Activity.this,cartBookingResponse.getOrder().getDetail());
                 recyclerView.setLayoutManager((new LinearLayoutManager(getApplicationContext())));
