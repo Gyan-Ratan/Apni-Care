27e16744b292c2530a7e4aa7aefff309ce0848fa
Harsh Tyagi <91258683+Mysharsh@users.noreply.github.com>
2022-12-27 19:53:13 +0530

    orderprescription.xml

--- app/src/main/java/com/example/apnicare/Upload_Prescription/sprescription.java
+++ app/src/main/java/com/example/apnicare/Upload_Prescription/sprescription.java
@@ -1,6 +1,5 @@
 package com.example.apnicare.Upload_Prescription;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
@@ -12,13 +11,12 @@ import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.apnicare.R;
-import com.example.apnicare.YourOrders.YourOrderConfirmedActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class sprescription extends AppCompatActivity {
 
     ConstraintLayout layout;
-    Button arrowBtn,proceed;
+    Button arrowBtn;
     FloatingActionButton btn;
     CardView expandableView;
 
@@ -29,7 +27,6 @@ public class sprescription extends AppCompatActivity {
         expandableView = findViewById(R.id.attachView);
         arrowBtn = findViewById(R.id.attach);
         layout = findViewById(R.id.ConstraintLayout);
-        proceed=findViewById(R.id.materialButton);
         btn=findViewById(R.id.floatingActionButton);
         arrowBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,17 +35,12 @@ public class sprescription extends AppCompatActivity {
                     TransitionManager.beginDelayedTransition(layout, new AutoTransition());
                     expandableView.setVisibility(View.VISIBLE);
                     btn.setImageResource(R.drawable.arrow_down);
-
                 } else {
                     TransitionManager.beginDelayedTransition(layout, new AutoTransition());
                     expandableView.setVisibility(View.GONE);
                     btn.setImageResource(R.drawable.arrow_up);
                 }
             }
-        });
-        proceed.setOnClickListener(view -> {
-            Intent intent = new Intent(sprescription.this, YourOrderConfirmedActivity.class);
-            startActivity(intent);
         });
 
     }
