65f566ac530d087bcde169eff1d8f63fb00b713f
Harshit jain <harshit2020cs198@abesit.edu.in>
2022-12-28 10:55:59 +0530

    navigation updated

--- app/src/main/java/com/example/apnicare/YourOrders/YourOrderConfirmedActivity.java
+++ app/src/main/java/com/example/apnicare/YourOrders/YourOrderConfirmedActivity.java
@@ -2,15 +2,28 @@ package com.example.apnicare.YourOrders;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.example.apnicare.MainActivity;
 import com.example.apnicare.R;
 
 public class YourOrderConfirmedActivity extends AppCompatActivity {
-
+    ImageView imgHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_your_order_confirmed);
+        imgHome=findViewById(R.id.imageViewtoHome);
+        imgHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(YourOrderConfirmedActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
     }
 }
