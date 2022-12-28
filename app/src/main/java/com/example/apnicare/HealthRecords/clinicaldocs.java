132d639880a560ac5c76efa03bcac64405381263
Harsh Tyagi <91258683+Mysharsh@users.noreply.github.com>
2022-12-28 14:38:06 +0530

    healthreports

---
+++ app/src/main/java/com/example/apnicare/HealthRecords/clinicaldocs.java
@@ -0,0 +1,19 @@
+package com.example.apnicare.HealthRecords;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.apnicare.R;
+
+
+public class clinicaldocs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clinicaldocs);
+
+
+    }
+}
