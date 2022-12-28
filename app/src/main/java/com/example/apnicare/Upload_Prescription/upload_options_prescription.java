4d5567e3ed8654c9bb224fa067d835e4c3562802
Harsh Tyagi <91258683+Mysharsh@users.noreply.github.com>
2022-12-28 10:55:45 +0530

    orderprescription

--- app/src/main/java/com/example/apnicare/Upload_Prescription/upload_options_prescription.java
+++ app/src/main/java/com/example/apnicare/Upload_Prescription/upload_options_prescription.java
@@ -3,6 +3,7 @@ package com.example.apnicare.Upload_Prescription;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -18,19 +19,27 @@ public class upload_options_prescription extends BottomSheetDialogFragment {
 
 
     BottomSheetBehavior bottomSheetBehavior;
-    LinearLayout camera;
+    LinearLayout camera,gallery,prescription;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_upload_options_prescription, container, false);
         camera=view.findViewById(R.id.uploadbycamera);
+        gallery=view.findViewById(R.id.uploadbygallery);
+        prescription=view.findViewById(R.id.uploadbyprevious);
         camera.setOnClickListener(view1 -> prescription_camera());
+        prescription.setOnClickListener(view1 -> savedprescription());
         return view;
 
     }
     public void prescription_camera(){
         Intent intent = new Intent(getActivity(),prescription_cam.class);
+        startActivity(intent);
+    }
+
+    public void savedprescription() {
+        Intent intent =new Intent(getActivity(),sprescription.class);
         startActivity(intent);
     }
 }
